23b6509610e9591bdf01de4d255db665f4c6e7d6
Lorenz Kuhn <lorenz.achim.kuhn@gmail.com>
2026-01-05 13:54:59 +0100

    move scripts to dedicated folder

---
+++ wsi-cbir/wsi-cbir/scripts/index_playground.py
@@ -0,0 +1,22 @@
+## Playground script to test functions of src.retrieval.index.Index outside docker.
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "."))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import faiss
+# from src.networks.encoder_mgmt import DIMS
+# from src.utils.metadata_filtration import load_filter_deps, get_meta_with_codes
+import numpy as np
+import json
+import pathlib as pl
+import random
+import time
+from src.config import CYTOMINE_CONFIG
+from src.networks.encoder_mgmt import DIMS
+from src.retrieval.index import Index
+
+if __name__ == '__main__':
+    os.makedirs(pl.Path('/home/lorenz/Repositories/cytomine/data/wsi-cbir/embeddings'), exist_ok=True)
+    index = Index(pl.Path('/home/lorenz/Repositories/cytomine/data/wsi-cbir/embeddings'), DIMS[CYTOMINE_CONFIG['encoder']]) if not (pl.Path('/home/lorenz/Repositories/cytomine/data/wsi-cbir/embeddings/index.faiss')).exists() else Index(pl.Path('/home/lorenz/Repositories/cytomine/data/wsi-cbir/embeddings')).load()
+    print('Index health check', index.is_healthy())
