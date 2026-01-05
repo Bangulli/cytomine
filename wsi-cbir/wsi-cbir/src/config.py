c66c503d9c9cb76fc771ef4976a9881fa82f3b0e
Lorenz Kuhn <lorenz.achim.kuhn@gmail.com>
2026-01-05 13:54:08 +0100

    change index saveing interval to 30 mins

--- wsi-cbir/wsi-cbir/src/config.py
+++ wsi-cbir/wsi-cbir/src/config.py
@@ -2,7 +2,7 @@ CYTOMINE_CONFIG={
     "encoder":'CHIEF', # the encoder architecture to use, currently only supports CHIEF
     "embeddings":'/embeddings', # where to store the embeddings at DATA_PATH
     "remove_bg":'dilated-otsu', # which background removal strategy to use
-    "level":1, # on which level to compute the embeddings
+    "level":1, # on which magnification level to compute the embeddings
     "full_precision":False, # use full precision
-    "index_saving_interval":300, # at which interval to save the index object at runtime
+    "index_saving_interval":1800, # at which interval to save the index object at runtime
 }
